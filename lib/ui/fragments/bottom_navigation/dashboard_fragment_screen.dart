46dc2bd3581eb3eb405a9377db323a9b44cb7a96
Foster Amponsah <foster.asante@gwosevo.com>
2026-07-14 16:56:30 +0000

    Update

--- lib/ui/fragments/bottom_navigation/dashboard_fragment_screen.dart
+++ lib/ui/fragments/bottom_navigation/dashboard_fragment_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
 import 'package:infoboxx/ui/fragments/bottom_navigation/favorite_fragment_screen.dart';
 import 'package:infoboxx/ui/fragments/bottom_navigation/home_fragment_screen.dart';
 import 'package:infoboxx/ui/fragments/bottom_navigation/profile_fragment_screen.dart';
@@ -12,13 +13,13 @@ class DashboardFragmentScreen extends StatefulWidget {
 }
 
 class _DashboardFragmentScreenState extends State<DashboardFragmentScreen> {
-  List<Widget> fragmentScreens = [
+  final List<Widget> _fragmentScreens = [
     HomeFragmentScreen(),
     FavoriteFragmentScreen(),
     ProfileFragmentScreen()
   ];
 
-  List navigationButtonsProperties = [
+  final List _navigationButtonsProperties = [
     {
       "active_icon": Icons.home,
       "non_active_icon": Icons.home_outlined,
@@ -36,11 +37,37 @@ class _DashboardFragmentScreenState extends State<DashboardFragmentScreen> {
     },
   ];
 
+  RxInt indexNumber = 0.obs;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("DASHBOARD FRAGMENT SCREEN"),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Obx(
+            () => _fragmentScreens[indexNumber.value]
+        ),
+      ),
+      bottomNavigationBar: Obx(
+          () => BottomNavigationBar(
+            currentIndex: indexNumber.value,
+            onTap: (val){
+              indexNumber.value = val;
+            },
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white24,
+            items: List.generate(_navigationButtonsProperties.length, (index) {
+             var navBtnProperty = _navigationButtonsProperties[index];
+             return BottomNavigationBarItem(
+               backgroundColor: Colors.black,
+               icon: Icon(navBtnProperty["non_active_icon"]),
+               activeIcon: Icon(navBtnProperty["active_icon"]),
+               label: navBtnProperty["label"],
+             );
+            }),
+          )
       ),
     );
   }
