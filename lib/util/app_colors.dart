02396d8bab026510ce7efcd82fb6706145e3e0a1
Foster Amponsah <foster.asante@gwosevo.com>
2026-07-16 11:48:28 +0000

    Update

---
+++ lib/util/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  //! Yellow colors
+  static const yellowColor = Color(0xFFFBC31B);
+
+  //! Dark colors
+  static const blackPure      = Color(0xFF000000);
+  static const blackCharcoal  = Color(0xFF121212);
+  static const blackEerie     = Color(0xFF1A1A1A);
+  static const blackGunMetal  = Color(0xFF212529);
+  static const blackOnyx      = Color(0xFF0B0C10);
+
+  //! White colors
+  static const whitePure      = Color(0xFFFFFFFF);
+  static const whiteCream     = Color(0xFFF8F9FA);
+  static const whiteSnow      = Color(0xFFFFFAFA);
+  static const whiteGhost     = Color(0xFFF8F8FF);
+
+  //! Gray colors
+  static const grayCoolSlate  = Color(0xFF707A8A);
+  static const grayMutedSteel = Color(0xFF9CA3AF);
+  static const grayLightSilver = Color(0xFFE5E7EB);
+  static const grayLight      = Color(0xFFF3F4F6);
+  static const grayLightCharcoal = Color(0xFF374151);
+}
