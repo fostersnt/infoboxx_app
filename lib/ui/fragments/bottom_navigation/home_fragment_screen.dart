4c99688ffd4bbd6f81eca984d44950581b1152f8
Foster Amponsah <foster.asante@gwosevo.com>
2026-07-17 10:44:18 +0000

    Update

--- lib/ui/fragments/bottom_navigation/home_fragment_screen.dart
+++ lib/ui/fragments/bottom_navigation/home_fragment_screen.dart
@@ -14,62 +14,14 @@ class HomeFragmentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ShimmerScope(
-      child: Row(
-        children: [
-          Expanded(
-            child: AppShimmers.circleShimmer(200),
-          ),
-          // const SizedBox(width: 12),
-          // Expanded(
-          //   child: ShimmerBox(
-          //     width: 150,
-          //     height: 150,
-          //     borderRadius: BorderRadius.circular(16),
-          //   ),
-          // ),
-          // const SizedBox(width: 12),
-          // // Expanded(child: ShimmerTextWidget(lines: 2, width: 140)),
-        ],
-      ),
-    );
-    // return UserCard();
-    // return ShimmerScope(
-    //   child: Scaffold(
-    //     appBar: AppBar(title: const Text("Users")),
-    //     body: Obx(
-    //       () => ShimmerKit(isLoading: _isLoading.value, child: UserCard()),
-    //     ),
-    //   ),
-    // );
-  }
-}
-
-class UserCard extends StatelessWidget {
-  const UserCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(16),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
+      child: Container(
+        color: AppColors.whitePure,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
         child: Row(
           children: [
-            CircleAvatar(radius: 30, backgroundColor: AppColors.redCrimson),
-            const SizedBox(width: 16),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "John Doe",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.yellowColor,
-                  ),
-                ),
-                SizedBox(height: 8),
-                Text("Software Engineer"),
-              ],
+            Expanded(
+              child: AppShimmers.circleShimmer(200),
             ),
           ],
         ),
